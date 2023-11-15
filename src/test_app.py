7a9bdca486cf1522ee2c42318424a9526c2d8a2e
Ignacio Bares <38444147+nachovoss@users.noreply.github.com>
2023-11-15 09:04:52 +0100

    add unittests

---
+++ src/test_app.py
@@ -0,0 +1,50 @@
+import io
+import os
+import app
+import unittest
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TEST_IMAGE_PATH = os.getenv('TEST_IMAGE_PATH')
+
+
+class TestImageCaptioningApp(unittest.TestCase):
+
+    def setUp(self):
+        app.app.testing = True
+        self.client = app.app.test_client()
+
+
+    def test_caption_endpoint_with_no_file(self):
+        response = self.client.post('/caption')
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('No file part', response.json['error'])
+
+
+    def test_caption_endpoint_with_invalid_file(self):
+        data = {
+            'file': (io.BytesIO(b"not an image"), 'test.txt')
+        }
+        response = self.client.post('/caption', content_type='multipart/form-data', data=data)
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('Invalid file type', response.json['error'])
+
+
+    def test_caption_endpoint_with_valid_file(self):
+        # Assuming there's a valid test image in the directory
+        with open(TEST_IMAGE_PATH, 'rb') as img:
+            img_bytes = io.BytesIO(img.read())
+        
+        data = {
+            'file': (img_bytes, 'test.jpg')
+        }
+        response = self.client.post('/caption', content_type='multipart/form-data', data=data)
+        self.assertEqual(response.status_code, 200)
+        print(response.json)
+        # Check if the response contains a caption (the exact content will depend on the model)
+        self.assertIn('caption', response.json)
+
+
+if __name__ == '__main__':
+    unittest.main()
