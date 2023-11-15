ac7fa9de970b8895acf1a6be8f1e626b86910013
Ignacio Bares <38444147+nachovoss@users.noreply.github.com>
2023-11-15 08:38:57 +0100

    add model to env and caption service

--- src/caption_service.py
+++ src/caption_service.py
@@ -1,15 +1,19 @@
 import re
+import os
 from PIL import Image
+from dotenv import load_dotenv
 from transformers import AutoProcessor, AutoModelForCausalLM
 from exceptions import ModelLoadError, CaptionGenerationError
 
+load_dotenv()
+MODEL=os.getenv("MODEL")
 class CaptionService:
     """Service for generating captions from images."""
 
     def __init__(self):
         try:
-            self.processor = AutoProcessor.from_pretrained("microsoft/git-base-coco")
-            self.model = AutoModelForCausalLM.from_pretrained("microsoft/git-base-coco")
+            self.processor = AutoProcessor.from_pretrained(MODEL)
+            self.model = AutoModelForCausalLM.from_pretrained(MODEL)
         except Exception as e:
             raise ModelLoadError("Failed to load the model and processor") from e
 
@@ -26,15 +30,16 @@ class CaptionService:
         if not isinstance(image, Image.Image):
             raise TypeError("Image must be a PIL image")
         try:
-            prompt = "describe the image"
+
+            prompt = "what is this?"
             inputs = self.processor(
                         prompt,
                         image,
                         return_tensors="pt",
-                        max_length=50
+                        max_length=64
                     )
 
-            sample = self.model.generate(**inputs, max_length=50)
+            sample = self.model.generate(**inputs, max_length=64)
             caption = self.processor.tokenizer.decode(sample[0]).replace(prompt, "")
             pattern = r"\[(SEP|CLS)\]"
             caption = re.sub(pattern, "", caption).strip().capitalize()
