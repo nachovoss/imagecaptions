59188ce75ef880a0cfbe97883f6c1519ed3d1a1b
Ignacio Bares <38444147+nachovoss@users.noreply.github.com>
2023-11-15 09:48:15 +0100

    add integration and  service tests

--- src/test_app.py
+++ src/test_app.py
@@ -2,7 +2,10 @@ import io
 import os
 import app
 import unittest
+import requests
+from PIL import Image
 from dotenv import load_dotenv
+from caption_service import CaptionService
 
 load_dotenv()
 
@@ -44,6 +47,46 @@ class TestImageCaptioningApp(unittest.TestCase):
         print(response.json)
         # Check if the response contains a caption (the exact content will depend on the model)
         self.assertIn('caption', response.json)
+
+
+class TestImageCaptioningAppIntegration(unittest.TestCase):
+    
+    BASE_URL = 'http://127.0.0.1:5000'
+
+    def test_caption_endpoint_with_no_file(self):
+        response = requests.post(f'{self.BASE_URL}/caption')
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('No file part', response.json()['error'])
+    
+
+    def test_caption_endpoint_with_invalid_file(self):
+        data = {
+            'file': (io.BytesIO(b"not an image"))
+        }
+        response = requests.post(f'{self.BASE_URL}/caption', files=data)
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('Invalid file type', response.json()['error'])
+    
+
+    def test_caption_endpoint_with_valid_file(self):
+        with open(TEST_IMAGE_PATH, 'rb') as img:
+            files = {'file': ('test.jpg', img)}
+            response = requests.post(f"{self.BASE_URL}/caption", files=files)
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('caption', response.json())
+
+
+class TestCaptionService(unittest.TestCase):
+
+    def setUp(self):
+        self.service = CaptionService()
+
+    def test_generate_caption_with_valid_image(self):
+        with open(TEST_IMAGE_PATH, 'rb') as img:
+            image = Image.open(io.BytesIO(img.read()))
+
+        caption = self.service.generate_caption(image)
+        self.assertIsInstance(caption, str)
 
 
 if __name__ == '__main__':
